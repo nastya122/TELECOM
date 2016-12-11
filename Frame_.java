33339a2649c48e4600a819499f0427ab3b48b133
L.C <anastasiagolovanyk@mail.ru>
2016-12-11 12:52:48 +0200

    Update Frame_.java

--- Frame_.java
+++ Frame_.java
@@ -8,7 +8,7 @@ public class Frame_ extends JFrame {
     public Frame_() {
         JFrame frame = new JFrame();
         frame.setTitle("TELECOM");
-  
+        JFrame.setDefaultLookAndFeelDecorated(true);
         Telecom_panel panel = new Telecom_panel();
         panel.setLayout(new GridLayout(6, 1));
         add(panel);
