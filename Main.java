9e65fa0eb3d79365b45d96202b16e0649f4123eb
L.C <anastasiagolovanyk@mail.ru>
2016-12-11 21:14:47 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,10 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        Frame_ frame = new Frame_();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Telecom_panel().setVisible(true);
+            }
+        });
     }
 }
